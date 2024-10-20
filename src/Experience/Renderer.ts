a20f47e2ae7eb18dffa82cc93790200baa6f04c4
rade <bajic.rade2@gmail.com>
2024-10-21 00:28:29 +0200

    Renderer properties and getters

--- src/Experience/Renderer.ts
+++ src/Experience/Renderer.ts
@@ -1,9 +1,17 @@
+import * as THREE from "three";
 import { Experience } from "./Experience";
+import type { Sizes } from "./Utils/Sizes";
+import type { Camera } from "./Camera";
 
 export class Renderer {
   private static instance: Renderer | null = null;
 
   private _experience: Experience;
+  //
+  private _canvas: HTMLCanvasElement;
+  private _sizes: Sizes;
+  private _scene: THREE.Scene;
+  private _camera: Camera | null;
 
   // ------------------------------------------------
   // ------------------------------------------------
@@ -18,6 +26,10 @@ export class Renderer {
 
   private constructor() {
     this._experience = Experience.getInstance();
+    this._camera = this._experience.camera;
+    this._canvas = this._experience.canvas;
+    this._scene = this._experience.scene;
+    this._sizes = this._experience.sizes;
   }
   // ------------------------------------------------
   // ------------------------------------------------
@@ -25,5 +37,17 @@ export class Renderer {
   // -----------------------
   get experience() {
     return this._experience;
+  }
+  get camera() {
+    return this._camera;
+  }
+  get canvas() {
+    return this._canvas;
+  }
+  get scene() {
+    return this._scene;
+  }
+  get sizes() {
+    return this._sizes;
   }
 }
